ef1283dd1bdef3f26ce4720865ee5bab2467fb3a
Ivan Gantsev <ivangancev@yandex.ru>
2026-05-20 20:11:56 +0300

    feat: display ASCII banner on --version flag

---
+++ src/Application.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Igancev\WorkReporter;
+
+use Symfony\Component\Console\Application as BaseApplication;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+final class Application extends BaseApplication
+{
+    public function doRun(InputInterface $input, OutputInterface $output): int
+    {
+        if (true === $input->hasParameterOption(['--version', '-V'], true)) {
+            Banner::render($output, $this->getVersion());
+
+            return 0;
+        }
+
+        return parent::doRun($input, $output);
+    }
+}
